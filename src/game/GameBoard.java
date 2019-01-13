4909eb4761909b46cdb3051be60ddb538da4f417
Joseph Golubchik <yosi_goloo@walla.co.il>
2019-01-13 12:18:50 +0200

    .

--- src/game/GameBoard.java
+++ src/game/GameBoard.java
@@ -25,7 +25,7 @@ public class GameBoard {
 	public final long ID_1 = 308019306; // Eli Haimov
 	public final long ID_2 = 209195353; // Yosef Golubchik
 	public final long ID_3 = 307993030; // Elad Cohen
-	
+
 	// Objects
 	private Player player;
 	private ArrayList<Packman> packmans;
@@ -35,20 +35,20 @@ public class GameBoard {
 	private A_Star_3 star3;
 	public Play play;
 	private Graph graph;
-	
+
 	// Map
 	private int height;
 	private int width;
 	private Point3D start;
 	private Point3D end;
 	public Point3D playerStart;
-	
+
 	// Flags
 	private boolean firstLoaded = false;
 	private boolean didFirstPath = false;
 	private boolean escaping = false;
 	public boolean manual = false;
-	
+
 	// Board Stats
 	private String game_file_name = "";
 	private double total_time = 0;
@@ -56,11 +56,11 @@ public class GameBoard {
 	private double killed_by_ghost = 0;
 	private double score = 0;
 	private double out_of_box = 0;
-	
-	
-//	private Point3D dest;
-//	private int dest_id = 0;
-	
+
+	private int escape_count;
+
+
+
 	/**
 	 * Creates a play object from a given map file path, then calls initBoard to initialize the gameBoard.
 	 * @param play contains information about the state of the game.
@@ -70,7 +70,7 @@ public class GameBoard {
 		game_file_name = map_file_path.substring(map_file_path.lastIndexOf('\\')+9, map_file_path.length()-4);
 		initBoard();
 	}
-	
+
 	/**
 	 * Initializes the GameBoard:
 	 * Sets the maps bounding box, the ID's, find's best starting point and sets it and calls loadBoard to do the rest.
@@ -78,28 +78,28 @@ public class GameBoard {
 	private void initBoard() {
 		height = gfx.Assets.map.getHeight();
 		width = gfx.Assets.map.getWidth();
-		
+
 		String map_data = play.getBoundingBox();
 		String[] words = map_data.split(",");
 		start = new Point3D(Double.parseDouble(words[2]), Double.parseDouble(words[3]));
 		end = new Point3D(Double.parseDouble(words[5]), Double.parseDouble(words[6]));
-		
+
 		play.setIDs(ID_1, ID_2, ID_3);
-		
+
 		loadBoard();
 		Point3D initLocation = pixelsToPoint(bestStartPoint());
 		play.setInitLocation(initLocation.x()+0.0005, initLocation.y()+0.0005);
-		
+
 		loadBoard();
 		graph = new Graph(this);
 	}
-	
+
 	public void nextMove() {
 		loadBoard();
 		playAlgo();
 		updateStats();
 	}
-	
+
 	/**
 	 * Starts the game.
 	 */
@@ -108,7 +108,7 @@ public class GameBoard {
 		firstLoaded = true;
 		didFirstPath = false;
 	}
-	
+
 	/**
 	 * Loads the gameBoard objects: packmans, fruits, ghosts and boxes using information from the play object.
 	 */
@@ -117,7 +117,7 @@ public class GameBoard {
 		ghosts = new ArrayList<Ghost>();
 		fruits = new ArrayList<Fruit>();
 		boxes = new ArrayList<Box>();
-		
+
 		ArrayList<String> board = play.getBoard();
 		Iterator<String> it = board.iterator();
 		while(it.hasNext()) {
@@ -168,7 +168,7 @@ public class GameBoard {
 		}
 		firstLoaded  = true;
 	}
-	
+
 	/**
 	 * Finds the best starting point for the player: the fruit which has the most close fruits to it.
 	 * @return Location of the best starting point.
@@ -203,17 +203,12 @@ public class GameBoard {
 	 */
 	public void playAlgo() {
 		if(!fruits.isEmpty()) {
-			if(!manual) {
-				calcPath();
-				calcAngle();
-				didFirstPath = true;
-			}
-			else {
-				calcAngleManual();
-			}
+			calcPath();
+			calcAngle();
+			didFirstPath = true;
 		}
 	}
-	
+
 	/**
 	 * Constructs an array containing the game statistics.
 	 * @return An array of the game statistics.
@@ -227,7 +222,7 @@ public class GameBoard {
 		stats[4] = out_of_box;
 		return stats;
 	}
-	
+
 	/**
 	 * Gets game statistics from play object and updates the board fields.
 	 */
@@ -240,7 +235,7 @@ public class GameBoard {
 		killed_by_ghost = Double.parseDouble(infos[14].substring(7, infos[14].length()-1));
 		out_of_box = Double.parseDouble(infos[17].substring(4));
 	}
-	
+
 	/**
 	 * Calculates the angle the player needs to move in next:
 	 * - If the player reached the last point of his current path, calculate a new path and reset dest_id.
@@ -249,38 +244,29 @@ public class GameBoard {
 	 * - The angle the player needs to go in is the azimuth between the players location and the next point's location.
 	 */
 	private void calcAngle() {
-			ArrayList<Point3D> path = star3.getPath();
-//			path.add(closestFruit());
-			path.remove(path.size()-1);
-			player.setPath(path);
-			if(player.getPath().size() - player.getDest_id() - 1 >= 0) {
-				player.setDest(player.getPath().get(player.getPath().size() - player.getDest_id() - 1));
-				Point3D dest_gis = pixelsToPoint(player.getDest());
-				Point3D player_gis = pixelsToPoint(player.getLocation());
-				int radius = 0;
-				// If close enough to current destination, start moving to next destination.
-				if((player.getLocation().ix() >= player.getDest().ix()-radius && player.getLocation().ix() <= player.getDest().ix()+radius) &&
-						(player.getLocation().iy() >= player.getDest().iy()-radius && player.getLocation().iy() <= player.getDest().iy()+radius)) {
-					player.setDest_id(player.getDest_id()+1);
-				}
-				player.angle = MyCoords.azimuth(player_gis, dest_gis);
-				play.rotate(player.angle);
+		ArrayList<Point3D> path = star3.getPath();
+		//			path.add(closestFruit());
+		path.remove(path.size()-1);
+		player.setPath(path);
+		if(player.getPath().size() - player.getDest_id() - 1 >= 0) {
+			player.setDest(player.getPath().get(player.getPath().size() - player.getDest_id() - 1));
+			Point3D dest_gis = pixelsToPoint(player.getDest());
+			Point3D player_gis = pixelsToPoint(player.getLocation());
+			int radius = 0;
+			// If close enough to current destination, start moving to next destination.
+			if((player.getLocation().ix() >= player.getDest().ix()-radius && player.getLocation().ix() <= player.getDest().ix()+radius) &&
+					(player.getLocation().iy() >= player.getDest().iy()-radius && player.getLocation().iy() <= player.getDest().iy()+radius)) {
+				player.setDest_id(player.getDest_id()+1);
 			}
-			else {
-				player.setDest_id(1);
-				calcPath();	
-			}
-	}
-	
-	private void calcAngleManual() {
-		Point3D dest_gis = pixelsToPoint(player.getDest());
-		Point3D player_gis = pixelsToPoint(player.getLocation());
-		if(dest_gis != player_gis) {
 			player.angle = MyCoords.azimuth(player_gis, dest_gis);
 			play.rotate(player.angle);
 		}
+		else {
+			player.setDest_id(1);
+			calcPath();	
+		}
 	}
-	
+
 	/**
 	 * Calls the A-Star algorithm to calculate the shortest path from the player to his destination:
 	 * - If there are no fruits left do nothing.
@@ -343,7 +329,7 @@ public class GameBoard {
 		}
 		return closest.getLocation();
 	}
-	
+
 	/**
 	 * Time it takes for player to get to given fruit.
 	 * @param fruit
@@ -387,7 +373,7 @@ public class GameBoard {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Checks if the player is within a square radius of any point of a box.
 	 * @param position Players position.
@@ -408,7 +394,7 @@ public class GameBoard {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Gets a point in latitude and longitude and returns a point in pixels on the image
 	 * We used stackoverflow.com/questions/38748832/convert-longitude-and-latitude-coordinates-to-image-of-a-map-pixels-x-and-y-coor
@@ -446,46 +432,7 @@ public class GameBoard {
 
 		return new Point3D(newLat, newLong);
 	}
-	
-	//////// Ghost functions ////////
-//	public boolean isOppositeAngle(Ghost ghost) {
-//		double ghost_angle = azimuth(player.getLocation(), ghost.getLocation());
-//		double angle_diff = Math.min(player.angle, ghost_angle)+180-Math.max(player.angle, ghost_angle);
-//		if(Math.abs(angle_diff) <= 30)
-//			return true;
-//		return false;
-//	}
-	
-//	public void escape() {
-//		double distance = 100;
-//		Point3D closest_ghost = closestGhost();
-//		double player_to_ghost_angle = azimuth(player.getLocation(), closest_ghost);
-//
-//		while(new_angle < player_to_ghost_angle - 50) {
-//			new_angle += 50;
-//			if(!goesIntoWall(new_angle, distance) )
-//		}
-//		
-//		
-//		
-//		Iterator<Ghost> ghost_it = ghosts.iterator();
-//		while(ghost_it.hasNext() && !escaping) {
-//			Ghost curr = ghost_it.next();
-//			escape_count = 0;
-//			if(isOppositeAngle(curr)) {
-//				double new_angle = player.angle - 70;
-//				if (new_angle >=360) new_angle = player.angle + 70;
-//				System.out.println("Current player angle: "+player.angle);
-//				player.angle = new_angle;
-//				System.out.println("changing angle, new angle: "+player.angle);
-//				escaping = true;
-//			}
-//		}
-//		escape_count++;
-//		System.out.println(escape_count);
-//		if(escape_count >= 60 || escape_count == 0)
-//			escaping = false;
-//	}
+
 
 	// Getters and Setters
 	public int getHeight() {
@@ -495,7 +442,7 @@ public class GameBoard {
 	public int getWidth() {
 		return width;
 	}
-	
+
 	public Player getPlayer() {
 		return player;
 	}
@@ -515,11 +462,11 @@ public class GameBoard {
 	public ArrayList<Box> getBoxes() {
 		return boxes;
 	}
-	
+
 	public boolean isFirstLoaded() {
 		return firstLoaded;
 	}
-	
+
 	public boolean isDidFirstPath() {
 		return didFirstPath;
 	}
